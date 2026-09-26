a8110a735f92d36c46938e0d7f83fccd8febd978
Anton Pozharskiy <apozharski@gmail.com>
2026-09-26 16:34:47 +0200

    use MAT

--- auxiliary/x_aux_ref.c
+++ auxiliary/x_aux_ref.c
@@ -1979,7 +1979,7 @@ void ROWPE(int kmax, int *ipiv, struct MAT *sA)
 	REF_ROWPE(kmax, ipiv, sA);
 	}
 
-void ROWPE_OFFSET(int kmax, int *ipiv, struct blasfeo_dmat *sA, int ai)
+void ROWPE_OFFSET(int kmax, int *ipiv, struct MAT *sA, int ai)
 	{
 	REF_ROWPE_OFFSET(kmax, ipiv, sA, ai);
 	}
